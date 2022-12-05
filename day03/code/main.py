205c213d16d402fc3108ce801a619c3676c7b40f
José Tomás Tocino <josetomas.tocino@gmail.com>
2022-12-05 01:07:44 +0100

    Commit day03, part two

--- day03/code/main.py
+++ day03/code/main.py
@@ -1,18 +1,37 @@
+from functools import reduce
 import logging
 
 logger = logging.getLogger(__name__)
 
+get_priority = lambda x: ord(x) - (96 if x.islower() else 38)
+
+
 def part_one(inp):
     acum = 0
 
-    get_priority = lambda x: ord(x) - (96 if x.islower() else 38)
-
     for line in inp.splitlines():
         left, right = line[:len(line) // 2], line[len(line) // 2:]
-        
+
         mismatched_item_type = (set(left) & set(right)).pop()
 
         acum += get_priority(mismatched_item_type)
 
     return acum
 
+
+def part_two(inp):
+    acum = 0
+
+    group = []
+
+    for line in inp.splitlines():
+        group.append(line)
+
+        if len(group) == 3:
+            mismatched_item_type = reduce(lambda a, b: a & b, (set(x) for x in group)).pop()
+
+            acum += get_priority(mismatched_item_type)
+
+            group = []
+    
+    return acum
--- day03/test/test_main.py
+++ day03/test/test_main.py
@@ -1,6 +1,6 @@
 import logging
 import os.path
-from day03.code.main import part_one
+from day03.code.main import part_one, part_two
 
 logger = logging.getLogger(__name__)
 local_path = os.path.abspath(os.path.dirname(__file__))
@@ -15,7 +15,9 @@ CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 def test_sample_input(caplog):
     caplog.set_level(logging.INFO)
+    
     assert(part_one(sample_input) == 157)
+    assert(part_two(sample_input) == 70)
     
 
 
@@ -25,3 +27,4 @@ def test_big_input(caplog):
         content = f.read()
 
         assert(part_one(content) == 8252)
+        assert(part_two(content) == 2828)
